bd80e10fee4dd31f4bb93868ad212dc8bf91d4f1
Anant Arbariya <anant.arbariy@gmail.com>
2020-09-11 17:56:38 +0530

    My eclipse Maven Project

---
+++ MavenPro/src/test/java/com/lti/test/TestArithemetic.java
@@ -0,0 +1,26 @@
+package com.lti.test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.lang.annotation.Target;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.lti.code.Arithemetic;
+
+public class TestArithemetic {
+	private Arithemetic arth;
+	
+	
+	@Before
+	public void init() {
+		arth =new Arithemetic();
+	}
+	
+	@Test
+	public void testSum() {
+		assertEquals(10, arth.sum(4,6));
+	}
+}
